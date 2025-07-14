419418038eedd863f29198a2d059af59123b830a
陈子龙 <3039277918@qq.com>
2025-07-14 23:27:52 +0800

    Update main.js

--- vue_m_popquiz/src/main.js
+++ vue_m_popquiz/src/main.js
@@ -3,7 +3,7 @@ import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
 import App from './App.vue'
 import router from './router'
-
+//主要功能逻辑
 
 const app = createApp(App)
 app.use(Antd)
